e6ef160e077faee3f7e1f5e45922a1cbdaaa7cd0
codecinn27 <codecinnpro@gmail.com>
2025-03-14 02:09:47 +0800

    update snake game part 1

--- Day_20_Snake_Game/lv1/main.py
+++ Day_20_Snake_Game/lv1/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time 
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,7 +9,7 @@ screen.title("Snake Game")
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+segments =[]
 
 for position in starting_positions:
     segment = Turtle("square")
@@ -17,12 +18,19 @@ for position in starting_positions:
     segment.goto(position)
     segments.append(segment)
 
-
-
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
-        screen.update()
+    screen.update()
+    
+    # for seg_num in range(start=2, stop=0, step = -1 ):
+    for seg_num in range(len(segments)-1, 0, -1 ):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    
+    # for seg in segments:
+    #     seg.forward(20)
+    # time.sleep(1)   
         
 screen.exitonclick()
